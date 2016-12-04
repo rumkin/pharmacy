6cac41075fb6649b53029b7c8d80cc53178195d4
Rumkin <dev@rumk.in>
2016-12-04 21:12:07 +0000

    deps,utils: add `validation-report`, bump version

--- src/pharmacy.js
+++ src/pharmacy.js
@@ -1,7 +1,7 @@
 // Pharmacy components:
 exports.Store = require('./store.js');
 exports.Recipe = require('./recipe.js');
-exports.Report = require('./report.js');
+exports.Report = require('validation-report');
 exports.Field = require('./field.js');
 exports.Rule = require('./rule.js');
 exports.Error = require('./error.js');
